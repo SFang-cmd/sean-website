400fb0499e03a58cb36a2b9b381d3d2b1b05585c
SFang-cmd <57546442+SFang-cmd@users.noreply.github.com>
2024-03-18 01:17:25 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,203 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
+
+window.onscroll = () => {
+  const sections = document.querySelectorAll("section");
+  const navLi = document.querySelectorAll(".navBar .container ul li");
+  var current = "";
+  sections.forEach((section) => {
+    console.log("bruh");
+    const sectionTop = section.offsetTop;
+    if (window.scrollY + 200 >= sectionTop) {
+      current = section.getAttribute("id"); }
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
+    }
+  });
+};
+
+
+function FontImport() {
+  return (
+    <div>
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+      <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"/>
+    </div>
+  )
+}
+
+function Header() {
+  return (
+    <a href="#app">
+      <header className="appHeader">
+          <h1 className="Sean">Sean Fang</h1>
+          <p className="Titles">Computer Science and Robotics</p>
+      </header>
+    </a>
+  )
+}
+
+function Navigation() {
+  return (
+    <nav className="navBar">
+      <div className="container">
+        <ul className="navList">
+          <li className="home active"><a href="#app">Home</a></li>
+          <li className="bio"><a href="#bio">About Me</a></li>
+          <li className="research"><a href="#research">Research</a></li>
+          <li className="projects"><a href="#projects">Projects</a></li>
+          <li className="experience"><a href="#experience">Experience</a></li>
+          <li className="news"><a href="#news">News</a></li>
+          <li className="resume"><a href="#resume">Resume</a></li>
+          <li className="contact"><a href="#contact">Contact Me</a></li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+function AppContent() {
+  return (
+  <div className="appContent">
+    <section id="home" className="homeDiv">
+      <Home/>
+    </section>
+    <section id="bio" className="bioDiv">
+      <Bio/>
+    </section>
+    <section id="research" className="researchDiv">
+      <Research/>
+    </section>
+    <section id="projects" className="projectDiv">
+      <Projects/>
+    </section>
+    <section id="experience" className="experienceDiv">
+      <Experience/>
+    </section>
+    <section id="news" className="newsDiv">
+      <News/>
+    </section>
+    <section id="resume" className="resumeDiv">
+      <Resume/>
+    </section>
+    <section id="contact" className="contactDiv">
+      <Contact/>
+    </section>
+  </div>
+  )
+}
+
+function Home() {
+  return (
+    <h2 href="home" className="home">
+      hello;
+    </h2>
+  )
+}
+
+function Bio() {
+  return (
+    <h2 className="bio">
+      My name is Sean Fang, born and raised out of Richmond, Virginia.<br/>
+      Currently, I'm pursuing a Computer Science Bachelor's and Robotics Master's Degree at the University of Pennsylvania. <br/>
+      My interests include entrepreneurship, computer design, piano, running, and reading. <br/>
+      "The Best Way to Predict the Future is to Invent it" <br/>
+      - Abraham Lincoln
+    </h2>
+  )
+}
+
+function Research() {
+  return (
+    <h2 className="research">
+      My research
+    </h2>
+  )
+}
+
+function Projects() {
+  return (
+    <h2 className="projects">
+      My projects
+    </h2>
+  )
+}
+
+function Experience() {
+  return (
+    <h2 className="experience">
+      My Job Experience
+    </h2>
+  )
+}
+
+function News() {
+  return (
+    <h2 className="news">
+      News about Sean
+    </h2>
+  )
+}
+
+function Resume() {
+  return (
+    <h2 className="resume">
+      Find my resume
+    </h2>
+  )
+}
+
+function Contact() {
+  return (
+    <h2 className="contact">
+      Contact me here
+    </h2>
+  )
+}
+
+function Footer() {
+  return (
+    <footer className="foot">
+      Stuff
+    </footer>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
+    <div id="app" className="App">
+      <div className="App-Background">
+        <div className="AppHeader">
+          <Header/>
+          <Navigation/>
+        </div>
+        <div className="AppContent">
+          <AppContent/>
+        </div>
+      </div>
+    </div>
+  );
+  // <div className="FooterContent">
+  // <Footer/>
+  // </div>
+}
+export default App;
+
+// App color scheme: https://coolors.co/c9cad9-d1d2f9-a3bcf9-7796cb-576490
+
+{/* <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is a test
         </p>
         <a
           className="App-link"
@@ -18,8 +208,4 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
-  );
-}
-
-export default App;
+    </div> */}
